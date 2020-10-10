527e2942dec63fff75c860bb42dc90460a0d8b7b
stefaleon <stefaleon@gmail.com>
2020-10-10 12:00:27 +0300

    Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Button href='#' variant='contained' color='secondary'>Hello World</Button>  */}
+        <Button onClick={() => alert('Hi!')}
+          // disabled
+          // style={{ fontSize: 48, color: 'green', background: 'red' }}
+          size='large'
+          variant='contained' color='secondary'>Hello World</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
