0825c2754e4673e37d440c1da3775984db877f48
stefaleon <stefaleon@gmail.com>
2020-10-10 13:14:47 +0300

    ButtonGroup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        <Button
-          startIcon={<SaveIcon />}
-          endIcon={<SaveIcon />}
-          size='large'
-          variant='contained' color='secondary'>Hello World</Button>
+        <ButtonGroup>
+          <Button
+            startIcon={<SaveIcon />}
+            size='large'
+            variant='contained' color='primary'>Save
+          </Button>
+          <Button
+            startIcon={<DeleteIcon />}
+            size='large'
+            variant='contained' color='secondary'>Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
