fd585bf2ec2be68d5734ef18095ea8d06a0a392e
stefaleon <stefaleon@gmail.com>
2020-10-10 13:03:27 +0300

    Icons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Button href='#' variant='contained' color='secondary'>Hello World</Button>  */}
-        <Button onClick={() => alert('Hi!')}
-          // disabled
-          // style={{ fontSize: 48, color: 'green', background: 'red' }}
+
+        <Button
+          startIcon={<SaveIcon />}
+          endIcon={<SaveIcon />}
           size='large'
           variant='contained' color='secondary'>Hello World</Button>
         <img src={logo} className="App-logo" alt="logo" />
